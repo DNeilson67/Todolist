30d293199579e1a55405fcb791c308b8ee0086b0
Davin Neilson <114067292+DNeilson67@users.noreply.github.com>
2024-02-29 18:27:37 +0700

    Add files via upload

---
+++ components/profile.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { FaCheck } from "react-icons/fa";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function profile(){
+    return (<>
+    <div>Test</div>
+    </>);
+}
+
+export default profile;
